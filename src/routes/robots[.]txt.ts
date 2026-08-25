f2bc20df5d0c1a8f213767aea682ff8515017891
minjunio <minjunnios@gmail.com>
2026-08-25 14:13:28 +0400

    Upload extracted project: src/routes/robots[.]txt.ts

---
+++ src/routes/robots[.]txt.ts
@@ -0,0 +1,25 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/robots.txt")({
+  server: {
+    handlers: {
+      GET: async ({ request }) => {
+        const origin = new URL(request.url).origin;
+        const body = `User-agent: *
+Allow: /
+Disallow: /admin
+Disallow: /orders
+Disallow: /api/
+
+Sitemap: ${origin}/sitemap.xml
+`;
+        return new Response(body, {
+          headers: {
+            "Content-Type": "text/plain; charset=utf-8",
+            "Cache-Control": "public, max-age=86400",
+          },
+        });
+      },
+    },
+  },
+});
